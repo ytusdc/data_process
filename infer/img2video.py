f4139125ed5578173106400043ed02c274467ba9
ytusdc <ytusdc@126.com>
2024-12-03 13:52:11 +0800

    增加注释

--- infer/img2video.py
+++ infer/img2video.py
@@ -12,7 +12,15 @@ from pathlib import Path
 (3840*2160) : 4K,      1.31
 """
 
-def images_to_video(image_folder, video_path, fps, size=None):
+def images_to_video(image_folder, video_path, fps=25, size=None):
+    """
+    Args:
+        image_folder: 图片文件路径
+        video_path:  生成视频文件路径
+        fps: 帧率，默认25
+        size: 生成视频尺寸
+    Returns:
+    """
     # 获取图片列表
     images = [img for img in os.listdir(image_folder) if img.endswith(".png") or img.endswith(".jpg")]
     images.sort()  # 确保图片按顺序排列
@@ -22,21 +30,10 @@ def images_to_video(image_folder, video_path, fps, size=None):
         frame = cv2.imread(os.path.join(image_folder, images[0]))
         height, width, layers = frame.shape
         size = (width, height)
-
-    #编码参数：
-    # cv2.VideoWriter_fourcc('M', 'P', '4', 'V') MPEG-4编码 .mp4  要限制结果视频的大小，这是一个很好的选择。
-    # cv2.VideoWriter_fourcc('X','2','6','4')   MPEG-4编码  .mp4  想限制结果视频的大小，这可能是最好的选择。
-    # cv2.VideoWriter_fourcc('I', '4', '2', '0'),该参数是YUV编码类型，文件名后缀为.avi   广泛兼容，但会产生大文件
-    # cv2.VideoWriter_fourcc('P', 'I', 'M', 'I'),该参数是MPEG-1编码类型，文件名后缀为.avi
-    # cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'),该参数是MPEG-4编码类型，文件名后缀为.avi  要限制结果视频的大小，这是一个很好的选择。
-    # cv2.VideoWriter_fourcc('T', 'H', 'E', 'O'),该参数是Ogg Vorbis,文件名后缀为.ogv
-    # cv2.VideoWriter_fourcc('F', 'L', 'V', '1'),该参数是Flash视频，文件名后缀为.flv
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')    # 使用 MPEG-4 视频编解码器, .mp4
-    # fourcc = cv2.VideoWriter_fourcc(*'X264')  # 使用 H.264 编解码器
-    # fourcc = cv2.VideoWriter_fourcc(*'avc1')  # 使用 H.264 编解码器 (AVC)
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # fourcc = cv2.VideoWriter_fourcc(*'X','2','6','4')
-
+    # 编码参数：
+    # fourcc = cv2.VideoWriter_fourcc(*'DIVX')  # 使用 H.264 编解码器 (AVC), 文件名后缀为 .avi
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 该参数是MPEG-4编码类型，文件名后缀为 .avi
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 使用 MPEG-4 视频编解码器, 文件名后缀为 .mp4
     # 创建视频写入对象
     video_write = cv2.VideoWriter(video_path, fourcc, fps, size)
     # 写入每一帧图片
@@ -47,6 +44,7 @@ def images_to_video(image_folder, video_path, fps, size=None):
     # 释放视频写入对象
     video_write.release()
 
+# 视频压缩
 def compress_video(video_path):
     # 可选：使用 ffmpeg 进行进一步压缩
     save_dir = str(Path(video_path).parent)
@@ -65,17 +63,15 @@ def compress_video(video_path):
     subprocess.run(ffmpeg_command)
     print("Further compression using ffmpeg completed.")
 
-
 def main():
-    image_folder = '/home/ytusdc/测试数据/10.11/20241010000825-20241010110825/result/output_segment_2'
+    image_folder = '/home/ytusdc/测试数据/10.11/20241010000825-20241010110825/result/ttt'
     video_path = '/home/ytusdc/测试数据/10.11/20241010000825-20241010110825/result/result.mp4'
     fps = 25  # 每秒帧数
     video_size = None
     video_size = (1920, 1080)
 
-    images_to_video(image_folder, video_path, fps, size=video_size)
-    compress_video(video_path)
-
+    images_to_video(image_folder, video_path, fps=fps, size=video_size)
+    # compress_video(video_path)
 
 if __name__ == "__main__":
     main()
--- infer/video_clip.py
+++ infer/video_clip.py
@@ -1,30 +1,31 @@
 import os
 from pathlib import Path
 import cv2
-
-
 def str2senond(str_time):
     """
     Args:
         str_time: str_time format is hh:mm:ss
+        输入格式：hh:mm:ss / mm:ss / ss
     """
     split_ls = str_time.strip().split(":")
     for i in split_ls:
         if not i.isnumeric():
             print(f"输入的时间中包含，非数字，{str_time}")
             return -1
-
+    # ss
     if len(split_ls) == 1:
         if int(str_time) >= 60:
             print(f"秒不能大于60，当前秒输入是： {str_time}")
             return -1
         return int(str_time)
+    # mm:ss
     elif len(split_ls) == 2:
         m, s = split_ls
         if int(m) >= 60 or int(m) >= 60:
             print(f"分钟和秒不能大于60，当前秒输入是, {m}:{s}")
             return -1
         return  int(m) * 60 + int(s)
+    # hh:mm:ss
     elif len(split_ls) == 3:
         h, m, s = str_time.strip().split(":")
         if int(m) >= 60 or int(m) >= 60:
@@ -45,7 +46,6 @@ def extract_video_segment(video_path, clip_video_path, start_time, end_time):
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
     print(f"FPS: {fps}, Width: {width}, Height: {height}")
 
     start_seconds = str2senond(start_time)
@@ -57,40 +57,34 @@ def extract_video_segment(video_path, clip_video_path, start_time, end_time):
     start_frame = int(start_seconds * fps)
     end_frame = int(end_seconds * fps)
 
-    # 设置视频读取位置到开始帧
-    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
-
     # 创建一个VideoWriter对象来保存输出视频
     fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 或者使用其他编码格式如 'mp4v'
-    out = cv2.VideoWriter(clip_video_path, fourcc, fps, (width, height))
+    video_write = cv2.VideoWriter(clip_video_path, fourcc, fps, (width, height))
 
+    # 设置视频读取位置到开始帧
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
     frame_index = start_frame
     while cap.isOpened() and frame_index <= end_frame:
         ret, frame = cap.read()
         if not ret:
             print("End of video or error reading frame.")
             break
-
-        out.write(frame)  # 写入帧到输出视频
+        video_write.write(frame)  # 写入帧到输出视频
         frame_index += 1
 
-        # 可选：显示处理进度
+        # 显示处理进度
         if frame_index % 100 == 0:
             print(f"Processing frame {frame_index}/{end_frame}")
-
     # 释放资源
     cap.release()
-    out.release()
+    video_write.release()
     print("Extraction complete.")
-
-
 
 if __name__ == '__main__':
 
     input_video_path = '/home/ytusdc/测试数据/10.11/20241010000825-20241010110825/路卡下_采选路卡_采选路卡，100.142_20241007125829_20241007151951_3219837.mp4'  # 输入视频路径
     output_video_path = '/home/ytusdc/测试数据/10.11/20241010000825-20241010110825/output_segment_2.avi'  # 输出视频路径
-    start_time = "0:45:20"  # 开始时间为5秒
-    end_time = "0:50:00"   # 结束时间为10秒
-
+    start_time = "0:50:25"  # 开始时间为5秒
+    end_time = "0:50:40"   # 结束时间为10秒
 
     extract_video_segment(input_video_path, output_video_path, start_time, end_time)
